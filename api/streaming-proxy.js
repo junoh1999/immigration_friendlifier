de62de24cc0cdd40ddad7771becc987f7cbce335
junoh1999 <157660909+junoh1999@users.noreply.github.com>
2025-04-04 18:47:06 -0700

    Diarize_REVERT IF NOT WORK

--- api/streaming-proxy.js
+++ api/streaming-proxy.js
@@ -70,8 +70,9 @@ module.exports = async (req, res) => {
           punctuate: true,
           smart_format: true,
           diarize: true,
-          min_speakers: 2,
-          max_speakers: 6,
+          model: "nova-2-phonecall",  // Specialized for phone conversations with speaker separation
+          min_speakers: 2,            // Assume at least 2 speakers
+          max_speakers: 4,            // Don't go beyond 4 speakers
           encoding: "linear16",
           sample_rate: 16000,
           channels: 1
